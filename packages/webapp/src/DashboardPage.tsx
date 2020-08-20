2f0ee42ba6ff9df709279050144e37ef74566dab
William Siqueira <william.fatecsjc@gmail.com>
2020-08-20 12:12:49 -0300

    Finishing the page

--- packages/webapp/src/DashboardPage.tsx
+++ packages/webapp/src/DashboardPage.tsx
@@ -1,19 +1,24 @@
 import * as React from "react";
-import { useCallback, useState, useEffect, useMemo } from "react";
+import { useState } from "react";
 import { embeddedRuntimeUrl } from "@dashbuilder-js/api";
 import { DashboardSelector } from "@dashbuilder-js/selector";
 
+const DB_USER = "admin";
+const DB_PASSWORD = "admin";
 const DB_URL = "http://localhost:8280";
 
 export function DashboardPage() {
   const [target, setTarget] = useState("");
+
   const onDashboardSelect = (db: string, page: string) => {
     const newUri = embeddedRuntimeUrl(DB_URL, db, page);
     setTarget(newUri);
   };
+  
   return (
     <div>
-      <DashboardSelector url={DB_URL} user="admin" password="admin" onDashboardSelected={onDashboardSelect} />
+      <h2>Select your dashboard:</h2>
+      <DashboardSelector url={DB_URL} user={DB_USER} password={DB_PASSWORD} onDashboardSelected={onDashboardSelect} />
       <br />
       <iframe frameBorder="0" src={target} width="100%" height="900" />
     </div>
